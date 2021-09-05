32af17a9e77710ce9934344edb8782a8e619dde1
kNzw7 <a49010@alunos.isel.pt>
2021-09-06 00:36:52 +0100

    teste

--- src/main/kotlin/Draw.kt
+++ src/main/kotlin/Draw.kt
@@ -1,23 +1,28 @@
 import pt.isel.canvas.*
 
-fun Connect4.drawStatus(cv:Canvas) {
-    cv.drawRect(0,cv.height-STATUS_BAR,cv.width,STATUS_BAR, 0x7777777)
-    cv.drawText(cv.width/2-RADIUS,cv.height-TEXT_BASE,"${this.score.first} -", RED)
-    cv.drawText(cv.width/2+18,cv.height-TEXT_BASE,"${this.score.second}", YELLOW)
+fun Connect4.drawStatus(cv: Canvas) {
+    cv.drawRect(0, cv.height - STATUS_BAR, cv.width, STATUS_BAR, 0x7777777)
+    cv.drawText(cv.width / 2 - RADIUS, cv.height - TEXT_BASE, "${this.score.first} -", RED)
+    cv.drawText(cv.width / 2 + 18, cv.height - TEXT_BASE, "${this.score.second}", YELLOW)
 }
 
-fun drawSpaces(cv : Canvas) {
+fun drawSpaces(cv: Canvas) {
     ALL_POSITIONS.map { cv.drawCircle(it.row * CIRCLE_SIDE + RADIUS, it.column * CIRCLE_SIDE + RADIUS, RADIUS, WHITE) }
 }
 
-fun Canvas.drawDisc (disc:Disc) {
-    drawCircle(disc.pos.row * CIRCLE_SIDE + RADIUS ,disc.pos.column * CIRCLE_SIDE + RADIUS, RADIUS ,if (disc.player==Player.RED) RED else YELLOW )
+fun Canvas.drawDisc(disc: Disc) {
+    drawCircle(
+        disc.pos.row * CIRCLE_SIDE + RADIUS,
+        disc.pos.column * CIRCLE_SIDE + RADIUS,
+        RADIUS,
+        if (disc.player == Player.RED) RED else YELLOW
+    )
 }
 
-    fun drawGrid(cv: Canvas) {
-        (CIRCLE_SIDE..cv.height step CIRCLE_SIDE).forEach { cv.drawLine(0, it, cv.width, it, BLACK, 1) }
-        (CIRCLE_SIDE..cv.width step CIRCLE_SIDE).forEach { cv.drawLine(it, 0, it, cv.height, BLACK, 1) }
-    }
+fun drawGrid(cv: Canvas) {
+    (CIRCLE_SIDE..cv.height step CIRCLE_SIDE).forEach { cv.drawLine(0, it, cv.width, it, BLUE, 1) }
+    (CIRCLE_SIDE..cv.width step CIRCLE_SIDE).forEach { cv.drawLine(it, CIRCLE_SIDE, it, cv.height, BLUE, 1) }
+}
 
 fun Canvas.drawArena() {
     drawRect(0, CIRCLE_SIDE, width, height - STATUS_BAR, BLUE)
